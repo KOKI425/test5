1e68eb97abe0d0cf88771220f7c74283c4b80ba9
DESKTOP-U8K0FRP\irima <dani_sniper425@yahoo.co.jp>
2020-10-02 12:50:30 +0900

    ようこそ！！！を追加

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'やあこんにちは',
+              'やあこんにちは！ようこそ！！！'
             ),
             Text(
               '$_counter',
